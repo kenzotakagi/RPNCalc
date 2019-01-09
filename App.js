154cabfd9cb21b2b521d1b49ed693316b4784117
KenzaburoTakagi <ken.takagi216@gmail.com>
2019-01-09 18:27:08 +0900

    5-1-4

--- App.js
+++ App.js
@@ -55,6 +55,76 @@ export default class App extends React.Component {
         btnEvent: () => { this.calcButton('+') },
       },
     ],
+    [
+      {
+        label: '7',
+        btnEvent: () => { this.valueButton('7') },
+      },
+      {
+        label: '8',
+        btnEvent: () => { this.valueButton('8') },
+      },
+      {
+        label: '9',
+        btnEvent: () => { this.valueButton('9') },
+      },
+      {
+        label: '-',
+        btnEvent: () => { this.calcButton('-') },
+      },
+    ],
+    [
+      {
+        label: '4',
+        btnEvent: () => { this.valueButton('4') },
+      },
+      {
+        label: '5',
+        btnEvent: () => { this.valueButton('5') },
+      },
+      {
+        label: '6',
+        btnEvent: () => { this.valueButton('6') },
+      },
+      {
+        label: '*',
+        btnEvent: () => { this.calcButton('*') },
+      },
+    ],
+    [
+      {
+        label: '1',
+        btnEvent: () => { this.valueButton('1') },
+      },
+      {
+        label: '2',
+        btnEvent: () => { this.valueButton('2') },
+      },
+      {
+        label: '3',
+        btnEvent: () => { this.valueButton('3') },
+      },
+    ],
+    [
+      {
+        label: '0',
+        btnEvent: () => { this.valueButton('0') },
+      },
+      {
+        label: '.',
+        btnEvent: () => { this.valueButton('.') },
+      },
+      {
+        label: '/',
+        btnEvent: () => { this.calcButton('/') },
+      },
+    ],
+    [
+      {
+        label: 'Enter',
+        btnEvent: () => { this.enterButton() },
+      },
+    ]
   ]
 
   valueButton = (value) => {
@@ -89,19 +159,25 @@ export default class App extends React.Component {
           <View style={ styles.buttonLine }>
             <CalcButtons buttons={ this.buttons[0] } />
           </View>
+          <View style={ styles.buttonLine }>
+            <CalcButtons buttons={ this.buttons[1] } />
+          </View>
+          <View style={ styles.buttonLine }>
+            <CalcButtons buttons={ this.buttons[2] } />
+          </View>
 
-          <View style={ styles.buttonLine }>
-          </View>
-          <View style={ styles.buttonLine }>
-          </View>
           <View style={ styles.lastButtonLinesContainer }>
             <View style={ styles.twoButtonLines }>
               <View style={ styles.buttonLine }>
+                <CalcButtons buttons={ this.buttons[3] } />
               </View>
               <View style={ styles.buttonLine }>
+                <CalcButtons buttons={ this.buttons[4] } />
               </View>
             </View>
+
             <View style={ styles.enterButtonContainer }>
+              <CalcButtons buttons={ this.buttons[5] } />
             </View>
           </View>
         </View>
