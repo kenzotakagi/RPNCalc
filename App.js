483ccbf37c078af2899c512d89c3c0eafd4acacd
KenzaburoTakagi <ken.takagi216@gmail.com>
2019-01-09 17:23:28 +0900

    5-1-2

--- App.js
+++ App.js
@@ -1,15 +1,42 @@
 import React from 'react';
-import { 
+import {
   StyleSheet,
   Text,
   View,
   Platform,
   StatusBar,
+  TouchableOpacity,
 } from 'react-native';
 
 const STATUSBAR_HEAIGHT = Platform.OS == 'ios' ? 20 : StatusBar.currentHeight;
 
+const CalcButton = (props) => {
+  const flex = props.flex ? props.flex : 1
+  return (
+    <TouchableOpacity
+      style={ [styles.calcButton, { flex: flex }] }
+      onPress={ () => { props.btnEvent() } }>
+      <Text style={ styles.calcButtonText }>{ props.label }</Text>
+    </TouchableOpacity>
+  )
+}
+
 export default class App extends React.Component {
+  valueButton = (value) => {
+  }
+
+  enterButton = () => {
+  }
+
+  calcButton = (value) => {
+  }
+
+  acButton = () => {
+  }
+
+  cButton = () => {
+  }
+
   render() {
     return (
       <View style={ styles.container }>
@@ -23,21 +50,25 @@ export default class App extends React.Component {
           </View>
         </View>
 
-        <View style={styles.buttons}>
-          <View style={styles.buttonLine}>
+        <View style={ styles.buttons }>
+          <View style={ styles.buttonLine }>
+            <CalcButton flex={ 2 } label={ 'AC' } btnEvent={ () => this.acButton() } />
+            <CalcButton label={ 'C' } btnEvent={ () => this.cButton() } />
+            <CalcButton label={ '+' } btnEvent={ () => this.calcButton('+') } />
           </View>
-          <View style={styles.buttonLine}>
+
+          <View style={ styles.buttonLine }>
           </View>
-          <View style={styles.buttonLine}>
+          <View style={ styles.buttonLine }>
           </View>
-          <View style={styles.lastButtonLinesContainer}>
-            <View style={styles.twoButtonLines}>
-              <View style={styles.buttonLine}>
+          <View style={ styles.lastButtonLinesContainer }>
+            <View style={ styles.twoButtonLines }>
+              <View style={ styles.buttonLine }>
               </View>
-              <View style={styles.buttonLine}>
+              <View style={ styles.buttonLine }>
               </View>
             </View>
-            <View style={styles.enterButtonContainer}>
+            <View style={ styles.enterButtonContainer }>
             </View>
           </View>
         </View>
@@ -87,5 +118,17 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     borderWidth: 1,
+  },
+  calcButton: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '100%',
+    height: '100%',
+    flexDirection: 'column',
+    borderWidth: 1,
+    borderColor: "#b0c4de",
+  },
+  calcButtonText: {
+    fontSize: 20,
   },
 });
